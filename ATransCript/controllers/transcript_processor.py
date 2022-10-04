0237de1a7db6cf2046b3376760978a03d3a96b6e
akhilATC <akhiltcheriyan002@gamil.com>
2022-10-04 12:23:28 +0530

    qfix

--- ATransCript/controllers/transcript_processor.py
+++ ATransCript/controllers/transcript_processor.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from .transcript_tasks import add_two
+from flask import current_app
 
 
 transcript_module = Blueprint("transcript_module", __name__, url_prefix="/transcript")
@@ -11,5 +12,23 @@ def trans_ping():
 
     :return:
     """
-    add_two.delay(10 ,20)
+    task_id = add_two.delay(10, 20)
+    print(task_id)
     return "DEMO"
+
+
+@transcript_module.route("/convert_transcript", methods=['POST'])
+def convert_transcript():
+    """
+
+    :return:
+
+    """
+    audio_file = request.files['file']
+    try:
+        file_extention = audio_file.filename.split('.')[-1].lower()
+        if file_extention not in current_app.config['ALLOWED_EXTENTIONS']:
+            return jsonify({"message": "Invalid file stream"})
+        return jsonify({"message": "cool"})
+    except Exception as e:
+        return jsonify({"message": "error"})
--- ATransCript/controllers/transcript_tasks.py
+++ ATransCript/controllers/transcript_tasks.py
@@ -1,4 +1,8 @@
 from factory import celery
+import speech_recognition as sr
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
 
 
 @celery.task()
@@ -6,3 +10,9 @@ def add_two(x, y):
     print("cool:::")
     print(x+y)
     return x + y
+
+def convert_speech(file_):
+    r = sr.Recognizer()
+    extention = file_.filename.split('.')[-1].lower()
+    if extention == 'mp4':
+        AudioSegment.from_file_using_temporary_files(file_)
